cd418694727c9006408d5ac2f36219c17141f836
Richard <richarddewan@hotmail.com>
2020-04-01 23:08:50 +0700

    refactot task view model and task fragment

--- app/src/main/java/com/dewan/todoapp/viewmodel/task/TaskViewModel.kt
+++ app/src/main/java/com/dewan/todoapp/viewmodel/task/TaskViewModel.kt
@@ -16,36 +16,37 @@ import com.dewan.todoapp.model.repository.AddTaskRepository
 import retrofit2.HttpException
 
 
-class TaskViewModel : ViewModel() {
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
 
     companion object {
         const val TAG = "TaskViewModel"
     }
 
     private val networkService = Networking.create(BuildConfig.BASE_URL)
-    private lateinit var addTaskRepository: AddTaskRepository
-    private lateinit var sharesPreferences: SharedPreferences
-    private lateinit var appPreferences: AppPreferences
+    private var addTaskRepository: AddTaskRepository
+    private var sharesPreferences = application.getSharedPreferences("com.dewan.todoapp.pref",Context.MODE_PRIVATE)
+    private var appPreferences: AppPreferences
     private var token: String = ""
-    val user_id: MutableLiveData<Int> = MutableLiveData()
+    val userId: MutableLiveData<Int> = MutableLiveData()
     val progress: MutableLiveData<Boolean> = MutableLiveData()
+    private val isSuccess: MutableLiveData<Boolean> = MutableLiveData()
+    val isError: MutableLiveData<String> = MutableLiveData()
 
-
-    fun init(context: Context){
+    init {
         addTaskRepository = AddTaskRepository(networkService)
-        sharesPreferences = context.getSharedPreferences("com.dewan.todoapp.pref",Context.MODE_PRIVATE)
         appPreferences = AppPreferences(sharesPreferences)
         token = appPreferences.getAccessToken().toString()
-        user_id.value = appPreferences.getUserId()
-
+        userId.value = appPreferences.getUserId()
     }
+
 
     fun addTask(addTaskRequest: AddTaskRequest) = liveData {
         try {
             progress.value = true
 
             val data =  addTaskRepository.addTask(token,addTaskRequest)
-            emit(data)
+            isSuccess.value = data.code() == 201
+            emit(isSuccess.value)
 
             progress.value = false
 
@@ -53,10 +54,12 @@ class TaskViewModel : ViewModel() {
         }
         catch (httpException: HttpException){
             Log.e(TAG,httpException.toString())
+            isError.value = httpException.toString()
 
         }
         catch (exception: Exception){
             Log.e(TAG,exception.toString())
+            isError.value = exception.toString()
         }
 
 
