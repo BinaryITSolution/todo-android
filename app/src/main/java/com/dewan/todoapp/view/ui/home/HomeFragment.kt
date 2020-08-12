55fd2201cc04aac4a3a3464b6a5977e18d538f73
Richard <richarddewan@hotmail.com>
2020-08-12 21:23:26 +0700

    firebase crashlytics

--- app/src/main/java/com/dewan/todoapp/view/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dewan/todoapp/view/ui/home/HomeFragment.kt
@@ -1,8 +1,7 @@
 package com.dewan.todoapp.view.ui.home
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,14 +16,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dewan.todoapp.R
 import com.dewan.todoapp.databinding.HomeFragmentBinding
 import com.dewan.todoapp.model.local.entity.TaskEntity
-import com.dewan.todoapp.model.remote.response.todo.TaskResponse
 import com.dewan.todoapp.view.adaptor.TaskAdaptor
 import com.dewan.todoapp.view.adaptor.TaskCallBack
-import com.dewan.todoapp.view.ui.auth.LoginActivity
 import com.dewan.todoapp.viewmodel.home.HomeViewModel
 import kotlinx.android.synthetic.main.home_fragment.*
 import org.jetbrains.anko.support.v4.alert
 import timber.log.Timber
+
 
 class HomeFragment : Fragment(), TaskCallBack {
 
